fd0cb244c471ea2a6acb90c9ebdceff9455a5211
lynnmengZzz <Lynn_Life@163.com>
2018-12-12 19:34:50 +0800

    first commit

---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="renderer" content="webkit">
+
+    <title> hAdmin- 主页</title>
+
+    <meta name="keywords" content="">
+    <meta name="description" content="">
+
+    <!--[if lt IE 9]>
+    <meta http-equiv="refresh" content="0;ie.html" />
+    <![endif]-->
+
+    <link rel="shortcut icon" href="favicon.ico"> <link href="{{asset('admin/css/bootstrap.min.css?v=3.3.6')}}" rel="stylesheet">
+    <link href="{{asset('admin/css/font-awesome.min.css?v=4.4.0')}}" rel="stylesheet">
+    <link href="{{asset('admin/css/animate.css')}}" rel="stylesheet">
+    <link href="{{asset('admin/css/style.css?v=4.1.0')}}" rel="stylesheet">
+</head>
+<body>
+@yield('content')
+
+<!-- 全局js -->
+<script src="{{asset('admin/js/jquery.min.js?v=2.1.4')}}"></script>
+<script src="{{asset('admin/js/bootstrap.min.js?v=3.3.6')}}"></script>
+<script src="{{asset('admin/js/plugins/metisMenu/jquery.metisMenu.js')}}"></script>
+<script src="{{asset('admin/js/plugins/slimscroll/jquery.slimscroll.min.js')}}"></script>
+<script src="{{asset('admin/js/plugins/layer/layer.min.js')}}"></script>
+
+<!-- 自定义js -->
+<script src="{{asset('admin/js/hAdmin.js?v=4.1.0')}}"></script>
+<script type="text/javascript" src="{{asset('admin/js/index.js')}}"></script>
+
+<!-- 第三方插件 -->
+<script src="{{asset('admin/js/plugins/pace/pace.min.js')}}"></script>
+</body>
+</html>
